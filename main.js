01226ee471603031beadf5d43a18d4f00c4ee0d1
Hans <mcmacker4@gmail.com>
2019-03-29 13:23:42 +0100

    Standalone Bot

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
-require("discord-games")('./config.json');
+global.config = require('./config.json')
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const messageHandler = require('./bot/events/message')
+client.on('message', (message) => {
+    if(!message.bot && message.content.startsWith(global.config.prefix)) {
+        messageHandler(message)
+    }
+})
+
+client.login(global.config.token)
